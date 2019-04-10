ce8dc4e2ac8180aa5b8afc0809b4d3fef6891fd3
Ruiyang Ding <dry950526@gmail.com>
2019-04-10 16:31:06 +0300

    refactoring dictionary's structure

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""
+Flask-Djangoify
+-------------
+
+This is the description for that library
+"""
+from setuptools import setup
+from flask_djangoify.utils import version
+
+
+setup(
+    name='Flask-Djangoify',
+    version=version,
+    url='https://github.com/JohnDing1995/flask-djangoify',
+    license='BSD',
+    author='Ruiyang Ding',
+    author_email='dry950526@gmail.com',
+    description='Using flask, in a django way ',
+    long_description=__doc__,
+    py_modules=['flask_djangoify'],
+
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'flask-login'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
